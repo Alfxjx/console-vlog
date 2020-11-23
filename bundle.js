b8fe82871c0b0f070182d47ce586dd1e7202df72
xujianxiang <xjxtju@163.com>
2020-11-23 10:18:44 +0800

    init rollup+babel

---
+++ bundle.js
@@ -0,0 +1,11 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var foo = 'hello world!';
+
+var main = function main() {
+  console.log("the answer is ".concat(foo));
+};
+
+exports.main = main;
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import foo from './foo.js';
+
+export const main = () => {
+  console.log(`the answer is ${foo}`);
+}
